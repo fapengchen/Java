def30a467d198c02e6d84bb0636bacc611f1c09e
fapeng <fapeng.chen@cutt.com>
2018-11-02 18:55:32 +0800

    Submit second days' homework

---
+++ Day03/Work_1.java
@@ -0,0 +1,31 @@
+class Work_1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+}
+
+class Work_2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+	}
+}
+class Work_3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("   J      A      V     V      A");
+		System.out.println("   J     A A      V   V      A A");
+		System.out.println("J  J    AAAAA      V V      AAAAA");
+		System.out.println(" JJ    A     A      V      A     A");
+	}
+}
