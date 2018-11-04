a9b9194eb2e4c324561fe6828df8e61ce8f963e0
Fapeng Chen <fapeng.chen@icloud.com>
2018-11-04 23:18:22 +0800

    day03 work1-11

--- Day03/Work_1.java
+++ Day03/Work_1.java
@@ -29,3 +29,74 @@ class Work_3
 		System.out.println(" JJ    A     A      V      A     A");
 	}
 }
+class Work_4
+{	
+	public static void main(String[] args)
+	{
+		System.out.println("a    a^2    a^3");
+		System.out.println("1    1      1");
+		System.out.println("2    4      8");
+		System.out.println("3    9      27");
+		System.out.println("4    16     64");
+	}
+
+}
+class Work_5
+{
+	public static void main(String[] args)
+	{
+		System.out.println("(9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5):");
+		System.out.println(((9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5)));
+	}
+}
+class Work_6
+{
+	public static void main(String[] args)
+	{
+		System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9");
+		System.out.println((1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+	}
+}
+class Work_7
+{
+    public static void main(String[] args)
+    {
+        System.out.println((4 * (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11))));
+        System.out.println((4 * (1.0 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13))));
+    }
+}
+class Work_8
+{
+    public static void main(String[] args)
+    {
+        System.out.println("perimeter:");
+        System.out.println(2 * 5.5 * 3.14);
+        System.out.println("area:");
+        System.out.println(5.5 * 5.5 * 3.14);
+    }
+}
+class Work_9
+{
+    public static void main(String[] args)
+    {
+        System.out.println("area:");
+        System.out.println(4.5*7.9);
+    }
+}
+
+class Work_10
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Average speed:");
+        System.out.println(((14/1.6)/(45*60+30))*3600);
+    }
+}
+class Work_12
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Average speed:");
+        System.out.println(((24*1.6)/(45*60+30))*3600);
+    }
+}
