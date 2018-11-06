d32c7e220a9e8aa81c3497606a6a35906978c066
Fapeng Chen <fapeng.chen@icloud.com>
2018-11-07 00:08:14 +0800

    Day04 Third submission

---
+++ Day04/DisplayTime.java
@@ -0,0 +1,18 @@
+package Day04;
+import java.util.Scanner;
+
+public class DisplayTime{
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an integer for seconds: ");
+		int seconds = input.nextInt();
+		
+		int minutes = seconds / 60;
+		int remainingSeconds = seconds % 60;
+		System.out.println(seconds + "seconds is " + minutes +
+				"miutes and " + remainingSeconds + " seconds" );
+		
+	}
+}
---
+++ Day04/Work_2_8.java
@@ -0,0 +1,12 @@
+package Day04;
+
+class Work_2 {
+	public static void main(String[] args)
+	{
+		double miles = 100;
+		final double KILOMETERS_PER_MILE = 1.609;
+		double kilometers = miles * KILOMETERS_PER_MILE;
+		System.out.println("kilometers : " + kilometers);
+		
+	}
+}
