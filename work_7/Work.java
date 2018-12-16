3da082d9df1e68f1929057f4f1cca4fb8eb67d26
Fapeng Chen <fapeng.chen@icloud.com>
2018-12-16 21:41:31 +0800

    work07 02

--- work_7/Work.java
+++ work_7/Work.java
@@ -76,5 +76,155 @@ class Work_3
 	public static void main(String[] args)
 	{
 		
+		System.out.print("Enter the integers between 1 and 100: ");
+
+		int[] numbers = input_numbers();
+		int[] groups = groups_number(numbers);
+		for (int i=0;i < groups.length;i++)
+		{
+			if (groups[i] > 0)
+			{
+				if (groups[i] == 1)
+					System.out.printf("%d occurs %d time\n",i,groups[i]);
+				else
+					System.out.printf("%d occurs %d times\n",i,groups[i]);
+			}
+		}
 	}
+
+	public static int[] input_numbers()
+	{
+		Scanner scanner = new Scanner(System.in);
+	    int[] num = new int[100];
+	    int i = 0;
+	    int input;
+	    while (i >= 0) 
+	    {
+	        input = scanner.nextInt();
+	        if (input != 0) 
+	        {
+	            num[i] = input;
+	            i++;
+	        } 
+	        else 
+	            break;
+	    }
+	    return num;
+	}
+	public static int[] groups_number(int[] numbers)
+	{
+		int[] groups = new int[101];
+
+		for (int i = 0; i < numbers.length;i++)
+		{
+			if (numbers[i] > 0)
+			{
+				groups[numbers[i]]++;
+			}
+		}
+		return groups;
+	}
+}
+
+class Work_4
+{
+public static void main(String[] args)
+	{
+		
+		System.out.print("The input results: ");
+
+		int[] numbers = input_numbers();
+		int avg = 0;
+		int sum = 0;
+		int n = 0 ;
+		for (int i=0;i < numbers.length;i++)
+		{
+			if (numbers[i] > 0)
+			{
+				sum += numbers[i];
+				n++;
+			}
+		}
+		avg = sum / n;
+
+		int greater = 0;
+		int smaller = 0;
+
+		for (int e: numbers)
+		{
+			
+			if (e > 0)
+			{
+				if (e > avg)
+					greater++;
+				else
+					smaller++;
+			}
+
+		}
+		System.out.println("greater: " + greater);
+		System.out.println("smaller: " + smaller);
+	}
+
+	public static int[] input_numbers()
+	{
+		Scanner scanner = new Scanner(System.in);
+	    int[] num = new int[100];
+	    int i = 0;
+	    int input;
+	    while (i >= 0) 
+	    {
+	        input = scanner.nextInt();
+	        if (input > 0) 
+	        {
+	            num[i] = input;
+	            i++;
+	        } 
+	        else 
+	            break;
+	    }
+	    return num;
+	}
+}
+
+class Work_5
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the numbers: ");
+		int[] numbers = new int[10];
+		int n = 0;
+		int s = 0;
+		for (int i = 0; i < numbers.length;i++)
+		{
+			n = input.nextInt();
+			if (repeatNumber(numbers,n))
+			{
+				numbers[i] = n;
+				s++;
+			}	
+
+		}
+		System.out.println("The number of distinct number is " + s);
+		System.out.print("The distinct numbers are: ");
+		for (int e:numbers)
+		{
+			if (e > 0)
+				System.out.print(" " + e);
+		}
+		System.out.println();
+	}
+	public static boolean repeatNumber(int[] numbers,int n)
+	{
+		for (int i = 0;i < numbers.length;i++)
+		{
+			if (n == numbers[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	} 
 }
