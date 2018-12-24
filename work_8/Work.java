7d7ee630d1b16d150f50cc90afc67986f382dcbb
fapeng <fapeng.chen@cutt.com>
2018-12-24 19:00:33 +0800

    work08 05

--- work_8/Work.java
+++ work_8/Work.java
@@ -233,3 +233,77 @@ class Work_5
 		return c;
 	}
 }
+
+class Work_6
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter matrix1: ");
+		double[][] matrix1 = new double[3][3];
+
+		for (int i = 0;i < matrix1.length;i++)
+		{
+			for (int j = 0; j < matrix1[i].length;j++)
+			{
+				matrix1[i][j] = input.nextDouble();
+			}
+		}
+		
+		System.out.print("Enter matrix2: ");
+		double[][] matrix2 = new double[3][3];
+
+		for (int i = 0;i < matrix2.length;i++)
+		{
+			for (int j = 0; j < matrix2[i].length;j++)
+			{
+				matrix2[i][j] = input.nextDouble();
+			}
+		}
+		double[][] matrix = multiplyMatrix(matrix1,matrix2);
+		for (int i = 0;i < matrix.length;i++)
+		{
+			for (int j = 0;j<matrix1[i].length;j++)
+			{
+				System.out.print(matrix1[i][j] + " ");
+			}
+
+			if (i == 1)
+				System.out.print(" " + "*"+ " ");
+			else
+				System.out.print(" " + " " + " ");
+			for (int j = 0;j<matrix2[i].length;j++)
+			{
+				System.out.print(matrix2[i][j] + " ");
+			}
+			if (i == 1)
+				System.out.print(" " + "="+ " ");
+			else
+				System.out.print(" " + " " + " ");
+			for (int j = 0;j<matrix[i].length;j++)
+			{
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	public static double[][] multiplyMatrix(double[][] a, double[][]b)
+	{
+		double[][] multiply = new double[a.length][a[0].length];
+
+		for (int i = 0;i < multiply.length;i++)
+			for (int j = 0;j < multiply[i].length;j++)
+			{	
+				double sum = 0;
+				for (int z = 0;z < 3;z++)
+				{
+					sum += a[i][z] * b[z][j];
+				}
+				multiply[i][j] = sum;
+			}
+		return multiply;
+	}
+}
